16eceef44684892ce9e94ba8978e9fc74d859ea8
Prasanna-Kalathy <prasannakalathy@gmail.com>
2022-10-18 17:26:31 +0530

    Init Parameters added by Web.XML to ReadUser

--- src/com/alpha/user/servlets/ReadUserServlet.java
+++ src/com/alpha/user/servlets/ReadUserServlet.java
@@ -1,10 +1,5 @@
 package com.alpha.user.servlets;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -13,18 +8,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.mysql.cj.x.protobuf.MysqlxSql.StmtExecute;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/ReadServlet")
+//@WebServlet("/ReadServlet")
 public class ReadUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection con;
 
-	public void init() {
+	public void init(ServletConfig config ) {
 		try {
 			System.out.println("init()");
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "student", "student");
+			con = DriverManager.getConnection(config.getInitParameter("dbUrl"),
+					config.getInitParameter("dbUser"),
+					config.getInitParameter("dbPass"));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
